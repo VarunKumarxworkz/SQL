1ee4c2484ee30ab53782a427dfb44229080ba5bf
VarunKumarxworkz <varunkumarxworkz@gmail.com>
2023-04-26 23:14:38 +0530

    Task

---
+++ Update.sql
@@ -0,0 +1,73 @@
+use  Gym;
+CREATE TABLE Gym_pro(ID int ,Name Varchar(20), Fees Bigint,Timing int,TrainerName varchar(20),CreatineCost int,Protiencost int,BCAACost int,
+EAACost int,MultivitamineCost int,FishOilCost int,ZMAcost int,Caffinecost int,DianabollCost int, HGHCost int,oilABasedCost int,DumbellCost int,
+Barbellcost int,Area varchar(20),State varchar(10));
+select * from Gym_pro;
+insert into Gym_pro values(1,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(2,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(3,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(4,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(5,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(6,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(7,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(8,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(9,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(10,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(11,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(12,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(13,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(14,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(15,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(16,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(17,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(18,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(19,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(20,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(21,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(22,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(23,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(24,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(25,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(26,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(27,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(28,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(29,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(30,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(31,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(32,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(33,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(34,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(35,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(36,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(37,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(38,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(39,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+insert into Gym_pro values(40,'varun',500,6,'varun',1500,4000,1800,2000,500,800,2000,800,1000,60,3000,12000,4999,'Banglore','karnataka');
+
+
+update Gym_pro set name = 'arun' where ID=2;
+update Gym_pro set Protiencost = 5000 where ID=2;
+update Gym_pro set BCAACost = 2000 where ID=2;
+update Gym_pro set EAACost = 1800 where ID=2;
+update Gym_pro set MultivitamineCost = 600 where ID=2;
+update Gym_pro set FishOilCost = 1000 where ID=2;
+update Gym_pro set Fees = 1000 where ID=2;
+update Gym_pro set Timing = 8 where ID=2;
+update Gym_pro set TrainerName = 'VIO' where ID=2;
+update Gym_pro set CreatineCost = 1000 where ID=2;
+update Gym_pro set ZMAcost = 900 where ID=2;
+update Gym_pro set Caffinecost = 600 where ID=2;
+update Gym_pro set DianabollCost = 1200 where ID=2;
+update Gym_pro set HGHCost = 70 where ID=2;
+update Gym_pro set oilABasedCost= 20000 where ID=2;
+update Gym_pro set DumbellCost = 4000 where ID=2;
+update Gym_pro set Barbellcost = 2000 where ID=2;
+update Gym_pro set Area = 'Ooty' where ID=2;
+update Gym_pro set State = 'TamilNadu' where ID=2;
+
+select * from Gym_pro;
+
+
+
+
+
